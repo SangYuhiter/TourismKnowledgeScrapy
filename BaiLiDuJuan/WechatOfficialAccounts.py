21863a27f8cc559cf83d880f872d25d301e8d402
SangyYuhiter <sangyu.code@gmail.com>
2023-06-22 11:29:04 +0800

    update scrach data in 2023.06.22

--- BaiLiDuJuan/WechatOfficialAccounts.py
+++ BaiLiDuJuan/WechatOfficialAccounts.py
@@ -24,7 +24,8 @@ WECHAT_SEARCH_PAGE_URL = "https://mp.weixin.qq.com/cgi-bin/searchbiz"
 WECHAT_ARTICLES_PAGE_URL = "https://mp.weixin.qq.com/cgi-bin/appmsg"
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
-    "Cookie": "appmsglist_action_3946503740=card; pac_uid=0_07feff2fa75e3; iip=0; pgv_pvid=1936248458; logTrackKey=e35157d2c7ec47a7a1ef5ff6246052ec; ua_id=4bpZ1czqYPI99twGAAAAACHpbCj7bRAT3xxAi9PPv1U=; wxuin=85875017099003; uuid=01963f8a2f172d8099e676cd355f1bc5; rand_info=CAESIOZ0e4Ix9TcbdZIERBrv75rpGOBlPk+aqQLZVi+yA4QT; slave_bizuin=3946503740; data_bizuin=3946503740; bizuin=3946503740; data_ticket=sS0/LtkhOfEvhDi5Knr06pckBTy7cdAPBNaKiTmksCrGLtT4J4NUROMq2N1lWLmt; slave_sid=ZGFnMW9CMXhjcW52M0pvSGMyM0ZEbTlBazZBSFhoQnp1X0tmTjlsYWxPcEVaNHZNWTY1TldDX1BFcFhfRlF3eUpaZnM1RThLZGJSbVludlJSSzFMWlJxREwxOGxObWZydHVya3RUWDIzcHFuOUFZVGRjSHVKNnR1cGdUR0lnT1dGOUlaSUptWHlsZ2ZmSmJZ; slave_user=gh_22b9ae63fe96; xid=da6fa6653543f08916f95bd32b884acf; mm_lang=zh_CN; _clck=3946503740|1|fcg|0; _clsk=1jq81kf|1686701030535|1|1|mp.weixin.qq.com/weheat-agent/payload/record",
+    # "Cookie": "appmsglist_action_3946503740=card; pac_uid=0_07feff2fa75e3; iip=0; pgv_pvid=1936248458; logTrackKey=e35157d2c7ec47a7a1ef5ff6246052ec; ua_id=4bpZ1czqYPI99twGAAAAACHpbCj7bRAT3xxAi9PPv1U=; wxuin=85875017099003; uuid=01963f8a2f172d8099e676cd355f1bc5; rand_info=CAESIOZ0e4Ix9TcbdZIERBrv75rpGOBlPk+aqQLZVi+yA4QT; slave_bizuin=3946503740; data_bizuin=3946503740; bizuin=3946503740; data_ticket=sS0/LtkhOfEvhDi5Knr06pckBTy7cdAPBNaKiTmksCrGLtT4J4NUROMq2N1lWLmt; slave_sid=ZGFnMW9CMXhjcW52M0pvSGMyM0ZEbTlBazZBSFhoQnp1X0tmTjlsYWxPcEVaNHZNWTY1TldDX1BFcFhfRlF3eUpaZnM1RThLZGJSbVludlJSSzFMWlJxREwxOGxObWZydHVya3RUWDIzcHFuOUFZVGRjSHVKNnR1cGdUR0lnT1dGOUlaSUptWHlsZ2ZmSmJZ; slave_user=gh_22b9ae63fe96; xid=da6fa6653543f08916f95bd32b884acf; mm_lang=zh_CN; _clck=3946503740|1|fcg|0; _clsk=1jq81kf|1686701030535|1|1|mp.weixin.qq.com/weheat-agent/payload/record",
+    "Cookie": "appmsglist_action_3946503740=card; pac_uid=0_07feff2fa75e3; iip=0; pgv_pvid=1936248458; logTrackKey=e35157d2c7ec47a7a1ef5ff6246052ec; ua_id=4bpZ1czqYPI99twGAAAAACHpbCj7bRAT3xxAi9PPv1U=; wxuin=85875017099003; mm_lang=zh_CN; uuid=0774d337b6d8992b12cde963d576afaf; rand_info=CAESIDXYvU2vi8HT3OZ0yLaAEi6itMPEqMQ5wtSXaH0d3iL1; slave_bizuin=3946503740; data_bizuin=3946503740; bizuin=3946503740; data_ticket=vQBxHgYKSmDS5/PDFBCOGSfuKqoqXYE+eiK8Axj3wle+01J2iWdeKdma8uXUTF2H; slave_sid=RjZ1bFNVMFp2d1BoYmdicVZ4NkxrYno1RVVNbEp2MjBUaHBjODJ6SHlXSkVoR2tQVTFtVVVvWkJ0VVRRX0pIVm5KaEhSRFZEeGhYVzYxZmJrcmxKQlduZTk0NGhzeWV3cFRHSWlaNzk1bXQ5OUZkMGZuMU1WR2N6SWhQbmd2RnBHSmh4MnczcFlzdGhQblpk; slave_user=gh_22b9ae63fe96; xid=5ace0a55f7925fc97338b7699426a05e; _clck=3946503740|1|fco|0; _clsk=16lzoyo|1687403343250|2|1|mp.weixin.qq.com/weheat-agent/payload/record",
     "Host": "mp.weixin.qq.com",
     "Referer": "https://mp.weixin.qq.com/"
 }
@@ -151,15 +152,19 @@ def get_wechat_information(nickname, store_path):
         print("login failed for {}".format(res.status_code))
 
 def get_main_information():
-    resource_path_1 = os.path.join(BAILIDUJUAN_1_OUT_PATH, STATIC_RESOURCE_PATH)
-    if not os.path.exists(resource_path_1):
-        os.makedirs(resource_path_1)
-    get_wechat_information(BAILIDUJUAN_NICKNAME_1, BAILIDUJUAN_1_OUT_PATH)
+    enable_path_1 = False
+    if enable_path_1:
+        resource_path_1 = os.path.join(BAILIDUJUAN_1_OUT_PATH, STATIC_RESOURCE_PATH)
+        if not os.path.exists(resource_path_1):
+            os.makedirs(resource_path_1)
+        get_wechat_information(BAILIDUJUAN_NICKNAME_1, BAILIDUJUAN_1_OUT_PATH)
 
-    resource_path_2 = os.path.join(BAILIDUJUAN_2_OUT_PATH, STATIC_RESOURCE_PATH)
-    if not os.path.exists(resource_path_2):
-        os.makedirs(resource_path_2)
-    get_wechat_information(BAILIDUJUAN_NICKNAME_2, BAILIDUJUAN_2_OUT_PATH)
+    enable_path_2 = True
+    if enable_path_2:
+        resource_path_2 = os.path.join(BAILIDUJUAN_2_OUT_PATH, STATIC_RESOURCE_PATH)
+        if not os.path.exists(resource_path_2):
+            os.makedirs(resource_path_2)
+        get_wechat_information(BAILIDUJUAN_NICKNAME_2, BAILIDUJUAN_2_OUT_PATH)
 
 
 def format_file_name(store_dir):
@@ -172,6 +177,6 @@ def format_file_name(store_dir):
         os.rename(old_file, new_file)
 
 if __name__ == "__main__":
-    # get_main_information()
-    format_file_name(BAILIDUJUAN_1_OUT_PATH)
-    format_file_name(BAILIDUJUAN_2_OUT_PATH)
+    get_main_information()
+    # format_file_name(BAILIDUJUAN_1_OUT_PATH)
+    # format_file_name(BAILIDUJUAN_2_OUT_PATH)
